c39ccd56ecb97f367fb9d663a7f27737bdca7aa3
SURKANTI SHIVA REDDY <161041025+SHIVAREDDY2005@users.noreply.github.com>
2025-12-24 15:20:47 +0530

    Add Padovan Sequence implementation
    Implement Padovan sequence calculation in C++.

---
+++ DP/Padovan Sequence.cpp
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/problems/padovan-sequence2855/1
+
+
+class Solution {
+  public:
+    int padovanSequence(int n) {
+        // code here.
+        vector<int>arr(n+1);
+        arr[0]=1;
+        arr[1]=1;
+        arr[2]=1;
+        if(n<=2)return 1;
+        for(int i=3;i<=n;i++){
+            arr[i]=(arr[i-2]+arr[i-3])%1000000007;
+        }
+        return arr[n];
+    }
+};
