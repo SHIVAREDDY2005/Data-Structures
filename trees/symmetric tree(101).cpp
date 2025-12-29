c85c5d9a226462007d0d9f0bd2b329f9a143a006
SURKANTI SHIVA REDDY <161041025+SHIVAREDDY2005@users.noreply.github.com>
2025-12-29 19:13:08 +0530

    Implement symmetric tree check in C++

---
+++ trees/symmetric tree(101).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(TreeNode* t1,TreeNode* t2){
+        if(!t1 && !t2)return true;
+        if(!t1 || !t2)return false;
+        if(t1->val!=t2->val)return false;
+        return check(t1->left,t2->right)&&check(t1->right,t2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root)return true;
+        return check(root->left,root->right);
+    }
+};
